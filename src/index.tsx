1ca8f15ff47ebd2c70a4001686f872a7b60e1d39
Rasskris <rassoshenko.kristina@yandex.ru>
2022-02-11 19:14:28 +0300

    init: setup project

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+import './index.css';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root,
+);
+
+reportWebVitals();
