050aaf4ace227fb4d0aef675504063006fd63a03
Rasskris <rassoshenko.kristina@yandex.ru>
2022-02-11 19:22:26 +0300

    feat: add app component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { BooksList, SearchBar } from './components';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <SearchBar />
+      <BooksList />
+    </div>
+  );
+};
+
+export default App;
